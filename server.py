20177b493ba8e7212574fdc31ef843629f7b1f2a
Jiaying Ji <ji000159@umn.edu>
2024-09-23 16:49:25 -0500

    add gunicorn

--- server.py
+++ server.py
@@ -4,5 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 @app.route('/<name>')
-def hello(name=None):
+def hello(name="Jiaying"):
     return render_template('hello.html', name=name)
+
+
+# if __name__ == "__main__":
+#     app.run(port=7000, debug=True)
